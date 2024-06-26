2a83291b81f7d01f043c9736e9a783dfb9ab3f3d
chessboy71 <bentegrimohamed@gmail.com>
2024-06-26 16:11:55 +0200

    s

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((vals) => {
-      console.log(vals[0].body, vals[1].firstName, vals[1].lastName);
-    })
-    .catch(() => {
+      console.log(`${vals[0].body} ${vals[1].firstName} ${vals[1].lastName}`);
+    }),
+    () => {
       console.log('Signup system offline');
-    });
+    };
 }
